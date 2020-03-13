7799c254e7cf16f30b5913bd3a6d4b86be4c617b
yassine <adil033@hotmail.com>
2020-03-13 05:54:58 -0400

    Rajout de la classe Adherent

---
+++ Adherent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_2
+{
+    // 1.1 classe Adherent 
+    class Adherent
+    {
+        private string nom;
+        private string prenom;
+        private int anneeAd;
+        private int code;
+    }
+}
