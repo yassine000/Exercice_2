52e9d56954b60ff8696dc3447d3fde2a166727e7
yassine <adil033@hotmail.com>
2020-03-12 20:51:16 -0400

    Ajoutez des fichiers projet.

---
+++ Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_2
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
