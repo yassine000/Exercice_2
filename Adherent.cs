74d43e1df107d5acb5f62bc336ef96fc691686f5
yassine <adil033@hotmail.com>
2020-03-13 06:56:51 -0400

    Methodes de la classe adherents

--- Adherent.cs
+++ Adherent.cs
@@ -14,14 +14,41 @@ namespace Exercice_2
         private int code;
         private int anneeAd;
         
-
+        // constructeur
         public Adherent(string nom, string prenom, int code, int anneeAd)
         {
             this.nom = nom;
             this.prenom = prenom;
-            this.anneeAd = anneeAd;
             this.code = code;
+            this.anneeAd = anneeAd;           
         }
-
+        // les méthode setCode(), getNom(), getPrenom() et getAnneeAd()
+        public void setCode(int newCode)
+        {
+            this.code = newCode;
+        }
+        public string getNom()
+        {
+            return nom;
+        }
+        public string getPrenom()
+        {
+            return prenom;
+        }
+        public int getAnneeAd()
+        {
+            return anneeAd;
+        }
+        // la méthode equals()
+        public bool equals(Adherent adherent_1, Adherent adherent_2)
+        {
+            return (adherent_1.code == adherent_2.code);                        
+        }
+        // la méthode  toString()
+        public string toString()
+        {
+            return "le nom de l'adherent est : " + nom + "\nle prénom de l'adherent est : " +
+                prenom + "\nle code est : " + code + "\nl'année d'adhesion est : " + anneeAd;
+        }
     }
 }
